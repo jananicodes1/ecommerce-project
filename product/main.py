dae3f8408dfe1fd862ec461de94793f24bac4200
jananicodes1 <janani22042005@gmail.com>
2026-07-14 07:58:14 +0530

    Fixed sys.path for Render deploy

--- product/main.py
+++ product/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
