9149de0cfd53070d2eb7e915b2f96a8b1c815a2c
jananicodes1 <janani22042005@gmail.com>
2026-07-15 09:53:42 +0530

    Fixed CORS for Netlify

--- product/main.py
+++ product/main.py
@@ -18,7 +18,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "*",
+        "https://storied-rugelach-26cf17.netlify.app",
+        "https://6a5645cb056d8ec84a563a01--storied-rugelach-26cf17.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
